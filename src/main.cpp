21bda06063794e43ef93d5dfbd9febad6e9b79b4
MuWuMu <90401149+MuWuMu@users.noreply.github.com>
2024-12-22 13:20:42 +0800

    adjust light pos and specular strength

--- src/main.cpp
+++ src/main.cpp
@@ -99,7 +99,7 @@ int main() {
     Wave wave(100, 100);
     Shader waterShader("shaders/wave.vs", "shaders/wave.fs");
     // light
-    glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+    glm::vec3 lightPos(30.0f, 1.0f, 30.0f);
 
     // set up vertex data (and buffers) and configure vertex attributes
     // ------------------------------------------------------------------
